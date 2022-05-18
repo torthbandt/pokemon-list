f3c295991e80dc50bd5f2eccdd8070058e92319f
Thomas Orthbandt <torthbandt@gmail.com>
2022-05-18 13:26:22 -0700

    Add css, js and html

---
+++ app.js
@@ -0,0 +1,38 @@
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex);
+
+const fetchPokemon = () => {
+     
+    const promises = [];
+
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then(( results ) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map( pokeman => `
+    <li class="card">
+        <img src="${pokeman.image}" class="card-image" />
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        <p class="card-subtitle">Type: ${pokeman.type}</p>
+    </li>
+    `)
+    .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
